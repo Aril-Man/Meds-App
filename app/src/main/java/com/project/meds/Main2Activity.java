6b2870adc39ee0d397594e1031acf8ab1d556620
Azrielfhr2 <azrielfhr23@gmail.com>
2020-11-21 13:35:52 +0700

    Upload Meds

---
+++ app/src/main/java/com/project/meds/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.project.meds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
